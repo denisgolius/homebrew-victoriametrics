41f04ad7aa400f0548e1080ed62fd37e6ae79546
Denys Holius <denaspireone@gmail.com>
2023-02-28 10:55:06 +0200

    make config as prometheus

--- victoriametrics.rb
+++ victoriametrics.rb
@@ -6,27 +6,29 @@ class Victoriametrics < Formula
   license "Apache-2.0"
   # head 'https://git@github.com:VictoriaMetrics/VictoriaMetrics.git'    
 
+  depends_on "gnu-tar" => :build
   depends_on "go" => :build
   depends_on "cmake" => :build
   
   def install
     ENV.deparallelize
     mkdir_p buildpath/"src/github.com/VictoriaMetrics"
-    ln_sf buildpath, buildpath/"src/github.com/VictoriaMetrics"
+    ln_sf buildpath, buildpath/"src/github.com/VictoriaMetrics/VictoriaMetrics"
 
     system "make", "victoria-metrics"
     bin.install "bin/victoria-metrics"
-    mkdir_p etc"/victoriametrics/vmsingle"
-    mkdir_p var"/log/victoriametrics/vmsingle"
-    mkdir_p var"/lib/victoriametrics-data"
+    # mkdir_p etc"/victoriametrics/vmsingle"
+    # mkdir_p var"/log/victoriametrics/vmsingle"
+    # mkdir_p var"/lib/victoriametrics-data"
 
     (bin/"victoriametrics_brew_services").write <<~EOS
       #!/bin/bash
-      exec #{bin}/victoria-metrics $(<#{etc}/victoriametrics/vmsingle/victoriametrics.args)
+      exec #{bin}/victoria-metrics $(<#{etc}/victoriametrics.args)
     EOS
 
     (buildpath/"victoriametrics.args").write <<~EOS
-      --config.file #{etc}/victoriametrics/vmsingle/scrape.yml --storageDataPath=/var/lib/victoria-metrics-data
+      --config.file #{etc}/scrape.yml
+      --storageDataPath=#{var}/victoriametrics-data
       --retentionPeriod=12
       --httpListenAddr=127.0.0.1:8428 
       --graphiteListenAddr=:2003 
@@ -51,15 +53,15 @@ class Victoriametrics < Formula
     <<~EOS
       When run from `brew services`, `victoriametrics` is run from
       `victoriametrics_brew_services` and uses the flags in:
-        #{etc}/victoria-metrics/vmsingle/vmsingle.args
+        #{etc}/vmsingle.args
     EOS
   end
 
   service do
     run [opt_bin/"victoriametrics_brew_services"]
     keep_alive false
-    log_path var/"log/victoriametrics/vmsingle/victoria-metrics.log"
-    error_log_path var/"log/victoriametrics/vmsingle/victoria-metrics.err.log"
+    log_path var/"log/victoria-metrics.log"
+    error_log_path var/"log/victoria-metrics.err.log"
   end
 
   test do
@@ -72,5 +74,5 @@ class Victoriametrics < Formula
       end
     end
   end
-  end
+end
   
